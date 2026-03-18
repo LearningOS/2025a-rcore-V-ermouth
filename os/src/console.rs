122d90dcc48c74afd4c2c9e3b7cc4a563d8e6bf2
YangKing <1659899019@qq.com>
2026-03-17 19:31:23 -0700

    学习+注释

--- os/src/console.rs
+++ os/src/console.rs
@@ -4,7 +4,10 @@ use core::fmt::{self, Write};
 
 struct Stdout;
 
+/// 实现Write trait，才能支持格式化输出
 impl Write for Stdout {
+    
+    /// 字符输出，调用SBI putchar输出格式化后的字符
     fn write_str(&mut self, s: &str) -> fmt::Result {
         for c in s.chars() {
             console_putchar(c as usize);
@@ -13,22 +16,23 @@ impl Write for Stdout {
     }
 }
 
-pub fn print(args: fmt::Arguments) {
+/// 格式化输出函数
+pub fn print_fmt(args: fmt::Arguments) {
     Stdout.write_fmt(args).unwrap();
 }
 
-/// Print! to the host console using the format string and arguments.
+/// 编写宏定义
 #[macro_export]
 macro_rules! print {
     ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::console::print(format_args!($fmt $(, $($arg)+)?))
+        $crate::console::print_fmt(format_args!($fmt $(, $($arg)+)?))
     }
 }
 
-/// Println! to the host console using the format string and arguments.
+/// 实现println宏，自动换行
 #[macro_export]
 macro_rules! println {
     ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::console::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?))
+        $crate::console::print_fmt(format_args!(concat!($fmt, "\n") $(, $($arg)+)?))
     }
 }
--- os/src/entry.asm
+++ os/src/entry.asm
@@ -1,12 +1,20 @@
+    # 定义一个代码段，名为.text.entry，专门放入口代码
     .section .text.entry
+
+    # 把_start符号导出为全局符号，让链接器能看到，作为程序入口
     .globl _start
 _start:
     la sp, boot_stack_top
     call rust_main
 
+    # 定义bss段，专门放栈空间（bss段是未初始化的内存，不会占用ELF文件体积）
     .section .bss.stack
+
+    # 导出栈的下界符号
     .globl boot_stack_lower_bound
 boot_stack_lower_bound:
     .space 4096 * 16
+    
+    # 导出栈的上界符号
     .globl boot_stack_top
 boot_stack_top:
